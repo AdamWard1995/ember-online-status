bff2c0b1f0c39e841af4b4eb2f52399ecd0a78a5
Adam Ward <adam.ward@carleton.ca>
2017-11-11 00:46:49 -0500

    [ci patch] try to fix npm error

--- deploy.sh
+++ deploy.sh
@@ -5,26 +5,27 @@ function bump {
   git config user.email "adam.ward@carleton.ca"
   git remote rm origin
   git remote add origin https://AdamWard1995:$GH_TOKEN@github.com/AdamWard1995/ember-online-status.git
+  git reset --hard HEAD
 
   echo "Bumping version..."
   VERSION="$(npm version $1 --force -m 'Bump to version %s')"
   COMMIT="$(git log -1)"
   echo "Git commit: $COMMIT"
-  if git rev-parse "$VERSION" >/dev/null 2>&1; then
-    echo "Pushing version changes..."
-    git push origin master
 
-    echo "Creating new release..."
-    API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
-    curl --data "$API_JSON" https://api.github.com/repos/AdamWard1995/ember-online-status/releases?access_token=$GH_TOKEN
+  echo "Pushing version changes..."
+  git push origin master
 
-    echo "Publishing to NPM..."
-    npm run ci-publish || true
-  fi
+  echo "Creating new release..."
+  API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
+  curl --data "$API_JSON" https://api.github.com/repos/AdamWard1995/ember-online-status/releases?access_token=$GH_TOKEN
+
+  echo "Publishing to NPM..."
+  npm run ci-publish || true
+
   exit 0
 }
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_NODE_VERSION" != "6.11.0" ] && [ "$EMBER_TRY_SCENARIO" != "ember-default" ]; then
   echo "Starting deployment process..."
   if [[ $TRAVIS_COMMIT_MESSAGE == *"[ci patch]"* ]]; then
     echo "Running patch release..."
